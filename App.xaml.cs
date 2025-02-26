e4a731805343e8b11f2e9938f64a2ba804c7f5fc
AurelaFetaj-RSA <l.fetaj@rsarobot.it>
2025-02-26 21:50:09 +0100

    changes

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Windows;
 
 namespace WPF_App
@@ -9,6 +10,15 @@ namespace WPF_App
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Force English on startup
+            CultureInfo culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            base.OnStartup(e);
+        }
     }
 
 }
--- Views/SettingsView.xaml.cs
+++ Views/SettingsView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_App.Views
 {
@@ -23,6 +12,57 @@ namespace WPF_App.Views
         public SettingsView()
         {
             InitializeComponent();
+            LoadLanguage("en-US"); // Set default language
         }
+
+        private void LanguageButton_Click(object sender, RoutedEventArgs e)
+        {
+            LanguagePopup.IsOpen = !LanguagePopup.IsOpen;
+        }
+
+        private void SetLanguage(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is string cultureCode)
+            {
+                LoadLanguage(cultureCode);
+                LanguagePopup.IsOpen = false;
+            }
+        }
+
+        private void LoadLanguage(string cultureCode)
+        {
+            try
+            {
+                // Create CultureInfo instance
+                CultureInfo culture = new CultureInfo(cultureCode);
+
+                // Create ResourceDictionary and load the appropriate language file
+                ResourceDictionary dict = new ResourceDictionary
+                {
+                    Source = new Uri($"/Resources/Localization/Dictionary-{cultureCode}.xaml", UriKind.Relative)
+                };
+
+                // Remove old dictionaries and add the new one
+                Application.Current.Resources.MergedDictionaries.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(dict);
+
+                // Set the application culture for UI threads
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+                // Debugging: Output to verify dictionary is loaded and culture is set
+                Console.WriteLine($"Language changed to: {cultureCode}");
+                Console.WriteLine($"Dictionary file used: /Resources/Localization/Dictionary-{cultureCode}.xaml");
+
+                // Refresh the UI to reflect changes
+                Application.Current.MainWindow?.UpdateLayout(); // Force layout update
+            }
+            catch (Exception ex)
+            {
+                // Show error message in case of failure
+                MessageBox.Show($"Error loading language: {ex.Message}");
+            }
+        }
+
     }
 }
